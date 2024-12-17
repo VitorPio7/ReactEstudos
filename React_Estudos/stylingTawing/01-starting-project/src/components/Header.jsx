2d5621759c073f067801aa8301e7d0a103dd4f83
Vitor Pio Vieira <106124241+VitorPio7@users.noreply.github.com>
2024-12-17 16:37:29 -0300

    s
    s

--- React_Estudos/stylingTawing/01-starting-project/src/components/Header.jsx
+++ React_Estudos/stylingTawing/01-starting-project/src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-16">
+    <header className="flex flex-col items-center mt-8 mb-8 mb:mb-16">
       <img src={logo} alt="A canvas" className="mb-8 w-44 object-contain" />
       <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
         ReactArt
