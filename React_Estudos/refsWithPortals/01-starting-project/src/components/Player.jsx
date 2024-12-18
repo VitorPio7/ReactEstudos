e9e932f7e151491bba993a2806e619c8a16485ce
Vitor Pio Vieira <106124241+VitorPio7@users.noreply.github.com>
2024-12-18 18:47:23 -0300

    today
    today

--- React_Estudos/refsWithPortals/01-starting-project/src/components/Player.jsx
+++ React_Estudos/refsWithPortals/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  let playerName = useRef();
+  let [name, setName] = useState("");
+
+  function changeOnClick() {
+    setName(playerName.current.value);
+    playerName.current.value = "";
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {name ? name : "unknown"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={changeOnClick}>Set Name</button>
       </p>
     </section>
   );
