b957338618700ab1a2e73d7fae27bc1943541a9c
Vitor Pio Vieira <106124241+VitorPio7@users.noreply.github.com>
2025-01-09 21:04:51 -0300

    today
    today

---
+++ React_Estudos/ResquestBack/01-starting-project/src/components/AvailablePlaces.jsx
@@ -0,0 +1,38 @@
+import Places from "./Places.jsx";
+import { useEffect, useState } from "react";
+import Modal from "./Modal.jsx";
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [AvailablePlaces, setAvailablePlaces] = useState();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+        if (response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError(error);
+      }
+    }
+    setIsFetching(false);
+    fetchPlaces();
+  }, []);
+  if (error) {
+    return <Modal title="An error occurred!" message={error.message} />;
+  }
+  return (
+    <Places
+      title="Available Places"
+      places={AvailablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
