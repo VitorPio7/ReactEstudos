cbce0c1d3c6a195fd0810df9b1ec4752ce90a1b0
VitorPio7 <vitorvpio60@gmail.com>
2025-03-25 21:15:56 -0300

    today
    today

--- React_Estudos/NextJSbigProgram/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
+++ React_Estudos/NextJSbigProgram/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
@@ -1,3 +1,18 @@
+import Link from 'next/link'
+import classes from './page.module.css'
 export default function MealDetailsPage(){
-    return <h1>Meal Details</h1>
+    return <>
+     <header className={classes.header}>
+        <h1>Delicious meals, created
+            <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Chose your favorite recipe!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+             Share Your Favorite recipe.
+          </Link>
+        </p>
+     </header>
+     <main className={classes.main}></main>
+    </>
 }
