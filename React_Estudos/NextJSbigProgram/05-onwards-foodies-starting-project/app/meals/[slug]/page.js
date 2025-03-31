5b75232801996fd267491aa2f3d66a7bd4a2e12e
VitorPio7 <vitorvpio60@gmail.com>
2025-03-31 15:33:37 -0300

    today
    today

--- React_Estudos/NextJSbigProgram/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
+++ React_Estudos/NextJSbigProgram/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
@@ -1,9 +1,20 @@
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
 import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
+
+export async function generateMetadata({params}) {
+  const meal = getMeal(params.mealSlug);
+  if(!meal){
+    notFound()
+  }
+  return {
+    title:meal.title,
+    description:meal.summary
+  };
+}
 
 export default  function MealDetailsPage({params}) {
-
   const meal =  getMeal(params.slug);
   meal.instructions = meal.instructions.replace(/\n/g,'<br/>');
     return <>
