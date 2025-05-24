1a3a5fd16a1490b9f6eb5032e1d40f5fd32597d2
VitorPio7 <vitorvpio60@gmail.com>
2025-05-23 21:45:07 -0300

    today

--- React_Estudos/React_with_TS/aula3/04 New Project Starting Code/src/components/Container.tsx
+++ React_Estudos/React_with_TS/aula3/04 New Project Starting Code/src/components/Container.tsx
@@ -1,10 +1,15 @@
-import { type ElementType } from "react"
+import {type ReactNode, type ElementType, type ComponentPropsWithoutRef } from "react"
 
-type ContainerProps={
-    as: ElementType /*permite receber um elemento no ts, como <Button/> */
-}
+type ContainerProps<T extends ElementType>={
+    as?: T; /*permite receber um elemento no ts, como <Button/> de forma genérica
+    e opcional*/
+    children:ReactNode;
+}&ComponentPropsWithoutRef<T>;
 
-export default function Container({as}:ContainerProps){
-    const Component = as;
-    return <Component/>
+export default function Container<C extends ElementType>
+({as,
+ children,
+...props}:ContainerProps<C>){
+    const Component = as|| 'div';
+    return <Component {...props}>{children}</Component>
 }
